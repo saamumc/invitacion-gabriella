7321bb2c6dbcae22e3d653f4c570637c7310da02
saamumc <saamumc@outlook.com>
2026-03-31 16:27:00 -0500

    Limpiar main y App de referencias a store

--- source/src/App.jsx
+++ source/src/App.jsx
@@ -1,43 +1,14 @@
-import React from "react";
-import styled from "styled-components";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AnimatedBackground from "./components/AnimatedBackground";
-import HomePage from "./pages/BirthdayInvitation";
+import React from 'react';
+import AnimatedBackground from './components/AnimatedBackground';
+import BirthdayInvitation from './pages/BirthdayInvitation';
 
-const AppContent = () => {
+function App() {
   return (
-    <AppContainer>
+    <>
       <AnimatedBackground />
-      <MainContent>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </MainContent>
-    </AppContainer>
+      <BirthdayInvitation />
+    </>
   );
-};
-
-const App = () => (
-  <Router>
-    <AppContent />
-  </Router>
-);
-
-const AppContainer = styled.div`
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  overflow: hidden;
-  /* 👇 CAMBIA ESTO 👇 */
-  background: transparent; 
-`;
-
-const MainContent = styled.div` 
-  width: 100%;
-  z-index: 10;
-  position: relative;
-`;
+}
 
 export default App;
--- source/src/main.jsx
+++ source/src/main.jsx
@@ -1,29 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-
-
-const container = document.getElementById("root");
-
-if (container) {
-  const root = createRoot(container);
-  root.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>
-  );
-} else {
-  console.error("No se encontró el elemento root. Verifica tu index.html");
-}
-
-// Limpieza para desarrollo (Vite/HMR)
-if (import.meta.hot) {
-  import.meta.hot.dispose(() => {
-    if (cleanup) cleanup();
-  });
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
